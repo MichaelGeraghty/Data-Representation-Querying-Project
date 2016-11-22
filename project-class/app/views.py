adf4f00b10968bee1e49e4cba81f7b826914b2e5
Michael Geraghty <geraghtymichael95@gmail.com>
2016-11-22 13:56:48 +0000

    Basic 8-ball functionality

--- project-class/app/views.py
+++ project-class/app/views.py
@@ -3,11 +3,24 @@ from flask import render_template,request
 
 from forms import GetQuestion
 
+from random import randint
+
 @app.route('/')
 def index():
 	return render_template('index.html')
-	
+
 @app.route('/8ball', methods=['GET', 'POST'])
 def get_question():
     form = GetQuestion()
-    return render_template('index.html', form=form)
+    if request.method == 'POST':
+        choice = randint(1,3)
+        if choice ==1:
+            response = "It is certain"
+        if choice ==2:
+            response = "It is decidedly so"
+        if choice ==3:
+            response = "Without a doubt"
+        return render_template('index.html', form=form,response=response )
+    if request.method == 'GET':	
+		return render_template('index.html', form=form)
+   
